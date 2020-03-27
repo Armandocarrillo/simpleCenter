7f574f21d221c7969fd7c8fca7cc5b43a7be466b
Armandocarrillo <ingcompuarmando@gmail.com>
2020-03-27 16:47:53 -0600

    Initial Commit

---
+++ simpleCenter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simpleCenter
+//
+//  Created by Armando Carrillo on 27/03/20.
+//  Copyright © 2020 Armando Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
